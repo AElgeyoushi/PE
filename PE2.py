922e399c049513248cfdf530ed37e59c0aecccbe
Amir Elgeyoushi <Amir.Elgeyoushi@hotmail.com>
2019-03-16 14:24:13 -0400

    Removed use of sets

--- PE2.py
+++ PE2.py
@@ -1,19 +1,13 @@
-even_fib = set()
 fib1 = 1
 fib2 = 2
-even_fib.add(fib2)
 fibsum = 0
+sum = 2
 
 while fibsum < 4000000:
 	fibsum = fib1 + fib2
 	if fibsum%2 == 0:
-		even_fib.add(fibsum)
+		sum += fibsum
 	fib1 = fib2
 	fib2 = fibsum
-	
-sum = 0
-while len(even_fib) != 0:
-	n = even_fib.pop()
-	sum += n
 	
 print(sum)
