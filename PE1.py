e39569088268e31ad4c459feba49e6167cda42d3
Amir Elgeyoushi <Amir.Elgeyoushi@hotmail.com>
2019-03-16 13:41:48 -0400

    Completed

---
+++ PE1.py
@@ -0,0 +1,23 @@
+mul_of_5 = set()
+mul_of_3 = set()
+complete = set()
+sum = 0
+
+i = 5
+while i < 1000:
+	mul_of_5.add(i)
+	i += 5
+	
+i = 3
+while i < 1000:
+	mul_of_3.add(i)
+	i += 3
+	
+complete = mul_of_5.union(mul_of_3)
+print(complete)
+
+while len(complete) != 0:
+	n = complete.pop()
+	sum += n
+
+print(sum)
